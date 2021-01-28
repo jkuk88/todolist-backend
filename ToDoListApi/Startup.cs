44349c266a98ba68b17ac8035f55b132195d57e8
József Kuk <jkuk@hotmail.hu>
2021-01-28 16:03:56 +0100

    EF service config

--- ToDoListApi/Startup.cs
+++ ToDoListApi/Startup.cs
@@ -1,17 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ToDoListApi.DataAccess.DataAccess;
 
 namespace ToDoListApi
 {
@@ -24,7 +18,6 @@ namespace ToDoListApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors(options =>
@@ -34,6 +27,7 @@ namespace ToDoListApi
                     builder => { builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); });
             });
 
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(Configuration.GetConnectionString("ApplicationDatabase")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -42,7 +36,7 @@ namespace ToDoListApi
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
