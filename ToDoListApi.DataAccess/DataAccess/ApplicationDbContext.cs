454a70593815e10c384255a41ddf799517156459
József Kuk <jkuk@hotmail.hu>
2021-01-28 16:02:03 +0100

    Entities and EF context added

---
+++ ToDoListApi.DataAccess/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoListApi.DataAccess.Entities;
+
+namespace ToDoListApi.DataAccess.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<ToDoListItem> ToDoListItems { get; set; }
+    }
+}
---
+++ ToDoListApi.DataAccess/Entities/ToDoListItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListApi.DataAccess.Entities
+{
+    public class ToDoListItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+    }
+}
